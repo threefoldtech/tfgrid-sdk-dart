616e195c26a93fc83290ed946f94e3e2b93e2493
AlaaElattar <alaamahmoud.1223@gmail.com>
2023-11-23 14:32:55 +0200

    add fromHexSeed

--- packages/signer/lib/signer.dart
+++ packages/signer/lib/signer.dart
@@ -2,5 +2,8 @@ library signer;
 
 import 'dart:typed_data';
 import 'package:polkadart_keyring/polkadart_keyring.dart';
+import 'package:hex/hex.dart';
+
+
 
 part 'src/sign.dart';
--- packages/signer/lib/src/sign.dart
+++ packages/signer/lib/src/sign.dart
@@ -1,32 +1,35 @@
 part of signer;
 
 class Signer {
-  KeyPair? _keyring;
+  KeyPair? _keypair;
 
   Future<void> fromMnemonic(String mnemonic) async {
-    _keyring = await KeyPair.fromMnemonic(mnemonic);
+    _keypair = await KeyPair.fromMnemonic(mnemonic);
   }
 
   Future<void> fromSeed(Uint8List seed) async {
-    _keyring = await KeyPair.fromSeed(seed);
+    _keypair = await KeyPair.fromSeed(seed);
+  }
+
+  Future<void> fromHexSeed(String seed) async {
+    final hexSeed = HEX.decode(seed.replaceAll('0x', ''));
+    _keypair = await KeyPair.fromSeed(Uint8List.fromList(hexSeed));
   }
 
   Future<Uint8List> sign(Uint8List data) async {
-    if (_keyring == null) {
-      throw Exception(
-          "Keyring not initialized. Call the appropriate constructor first.");
+    if (_keypair == null) {
+      throw Exception("Keypair not initialized.");
     }
 
-    final signature = await _keyring!.sign(data);
+    final signature = await _keypair!.sign(data);
     return signature;
   }
 
   Future<bool> verify(Uint8List signature, Uint8List data) async {
-    if (_keyring == null) {
-      throw Exception(
-          "Keyring not initialized. Call the appropriate constructor first.");
+    if (_keypair == null) {
+      throw Exception("Keypair not initialized.");
     }
 
-    return await _keyring!.verify(data, signature);
+    return await _keypair!.verify(data, signature);
   }
 }
