0535d4d36176e5c8c20c4c67e1594abb9661ee80
AlaaElattar <alaamahmoud.1223@gmail.com>
2023-11-23 15:08:31 +0200

    handle catching all errors

--- packages/signer/lib/src/sign.dart
+++ packages/signer/lib/src/sign.dart
@@ -4,32 +4,50 @@ class Signer {
   KeyPair? _keypair;
 
   Future<void> fromMnemonic(String mnemonic) async {
-    _keypair = await KeyPair.fromMnemonic(mnemonic);
+    try {
+      _keypair = await KeyPair.fromMnemonic(mnemonic);
+    } catch (e) {
+      throw Exception("Failed to create keypair from mnemonic. Error: $e");
+    }
   }
 
   Future<void> fromSeed(Uint8List seed) async {
-    _keypair = await KeyPair.fromSeed(seed);
+    try {
+      _keypair = await KeyPair.fromSeed(seed);
+    } catch (e) {
+      throw Exception("Failed to create keypair from seed. Error: $e");
+    }
   }
 
   Future<void> fromHexSeed(String seed) async {
-    final hexSeed = HEX.decode(seed.replaceAll('0x', ''));
-    _keypair = await KeyPair.fromSeed(Uint8List.fromList(hexSeed));
+    try {
+      final hexSeed = HEX.decode(seed.replaceAll('0x', ''));
+      _keypair = await KeyPair.fromSeed(Uint8List.fromList(hexSeed));
+    } catch (e) {
+      throw Exception("Failed to create keypair from hex seed. Error: $e");
+    }
   }
 
   Future<Uint8List> sign(Uint8List data) async {
     if (_keypair == null) {
       throw Exception("Keypair not initialized.");
     }
-
-    final signature = await _keypair!.sign(data);
-    return signature;
+    try {
+      final signature = await _keypair!.sign(data);
+      return signature;
+    } catch (e) {
+      throw Exception("Failed to sign data. Error: $e");
+    }
   }
 
   Future<bool> verify(Uint8List signature, Uint8List data) async {
     if (_keypair == null) {
       throw Exception("Keypair not initialized.");
     }
-
-    return await _keypair!.verify(data, signature);
+    try {
+      return await _keypair!.verify(data, signature);
+    } catch (e) {
+      throw Exception("Failed to verify signature. Error: $e");
+    }
   }
 }
