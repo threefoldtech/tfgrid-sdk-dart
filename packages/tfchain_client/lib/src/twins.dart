a3645fceadf4c92d7af824f62c7beb77bd455e9b
AlaaElattar <alaamahmoud.1223@gmail.com>
2023-11-30 15:35:14 +0200

    remove unused import

--- packages/tfchain_client/lib/src/twins.dart
+++ packages/tfchain_client/lib/src/twins.dart
@@ -1,5 +1,4 @@
 import 'package:tfchain_client/generated/dev/types/pallet_tfgrid/types/twin.dart';
-import 'package:tfchain_client/generated/dev/types/sp_core/crypto/account_id32.dart';
 import 'package:tfchain_client/tfchain_client.dart';
 
 class QueryTwins {
@@ -12,7 +11,8 @@ class QueryTwins {
   }
 
   Future<int?> getTwinIdByAccountId({required List<int> accountId}) async {
-    final res = await client.api.query.tfgridModule.twinIdByAccountID(accountId);
+    final res =
+        await client.api.query.tfgridModule.twinIdByAccountID(accountId);
     return res;
   }
 }
