45132506701a45a318b8a1730f86fab370666f9c
AlaaElattar <alaamahmoud.1223@gmail.com>
2024-04-17 14:07:29 +0200

    add kvStore module

--- packages/tfchain_client/bin/tfchain_client.dart
+++ packages/tfchain_client/bin/tfchain_client.dart
@@ -2,14 +2,15 @@ import 'dart:io';
 
 import 'package:signer/signer.dart';
 import 'package:tfchain_client/models/balances.dart';
+import 'package:tfchain_client/models/kvstore.dart';
 import 'package:tfchain_client/models/twins.dart';
 import 'package:tfchain_client/tfchain_client.dart';
 
 void main() async {
-  final queryClient = QueryClient("wss://tfchain.dev.grid.tf/ws");
-  await queryClient.connect();
+  // final queryClient = QueryClient("wss://tfchain.dev.grid.tf/ws");
+  // await queryClient.connect();
 
-  final twin = await queryClient.twins.get(QueryTwinsGetOptions(id: 214));
+  // final twin = await queryClient.twins.get(QueryTwinsGetOptions(id: 214));
 
   // final twinId = await queryClient.twins.getTwinIdByAccountId(
   //     QueryTwinsGetTwinByAccountIdOptions(accountId: twin!.accountId));
@@ -21,11 +22,12 @@ void main() async {
       "wss://tfchain.dev.grid.tf/ws",
       "secret add bag cluster deposit beach illness letter crouch position rain arctic",
       "sr25519");
-  client.connect();
+  await client.connect();
 
-  final extrinsic = await client.clientTwins
-      .update(TwinOptions(relay: twin!.relay ?? [], pk: twin.pk ?? []));
+  // final extrinsic =
+  //     client.kvStrore.set(KVStoreSetOptions(key: "mnemonic", value: "value"));
+  // await client.apply(extrinsic);
+  client.kvStrore.list();
 
-  await client.apply(extrinsic);
   // await client.disconnect();
 }
