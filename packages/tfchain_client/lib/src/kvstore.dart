e3b05570b878b6b9c605884f144cd53f808af8f6
AlaaElattar <alaamahmoud.1223@gmail.com>
2024-04-24 14:20:49 +0200

    add listall and delete all for kvStore

--- packages/tfchain_client/lib/src/kvstore.dart
+++ packages/tfchain_client/lib/src/kvstore.dart
@@ -1,7 +1,5 @@
-import 'dart:convert';
-
-import 'package:tfchain_client/generated/dev/dev.dart';
-import 'package:tfchain_client/generated/dev/types/tfchain_runtime/runtime.dart';
+import 'dart:typed_data';
+import 'package:polkadart/polkadart.dart';
 import 'package:tfchain_client/generated/dev/types/tfchain_runtime/runtime_call.dart';
 import 'package:tfchain_client/models/kvstore.dart';
 import 'package:tfchain_client/tfchain_client.dart';
@@ -25,16 +23,53 @@ class KVStore {
 
   Future<String> get(KVStoreGetOptions options) async {
     final res = await client.api.query.tFKVStore
-        .tFKVStore(client.keypair!.publicKey.bytes, options.key.codeUnits);
+        .tFKVStore(client.keypair!.publicKey.bytes, []);
+    print(res);
     return String.fromCharCodes(res);
   }
 
-  void list() async {
-    // final queries = client.api.query.
-    // final res = await queries.tFKVStore.
+  Future<Map<String, String>> list() async {
+    final moduleHash =
+        Hasher.twoxx128.hash(Uint8List.fromList('TFKVStore'.codeUnits));
+
+    final functionHash =
+        Hasher.twoxx128.hash(Uint8List.fromList('TFKVStore'.codeUnits));
+
+    final keyHash = Hasher.blake2b128
+        .hash(Uint8List.fromList(client.keypair!.publicKey.bytes));
+
+    Uint8List partialKey = Uint8List.fromList([
+      ...moduleHash,
+      ...functionHash,
+      ...keyHash,
+      ...client.keypair!.publicKey.bytes
+    ]);
+
+    final keys = await client.api.rpc.state.getKeys(key: partialKey);
+
+    final keysValues = await client.api.rpc.state.queryStorageAt(keys);
+
+    Map<String, String> keyValueMap = {};
+
+    for (var change in keysValues[0].changes) {
+      final key = change.key;
+      var value = change.value;
+
+      final added = key.sublist(partialKey.length + 17);
+
+      value = value!.sublist(1);
+      keyValueMap[String.fromCharCodes(added)] = String.fromCharCodes(value);
+    }
+
+    return keyValueMap;
   }
 
-  void deleteAll(){
+  void deleteAll() async {
+    Map<String, String> keys = await list();
 
+    for (String key in keys.keys) {
+      final extrinsic = delete(KVStoreGetOptions(key: key));
+      await client.apply(extrinsic);
+    }
   }
 }
