952dea2c1ce0f1d417d059f14293617c6c4f113b
AlaaElattar <alaamahmoud.1223@gmail.com>
2024-07-15 14:58:36 +0300

    fix swap to stellar test

--- packages/tfchain_client/test/tft_bridge_test.dart
+++ packages/tfchain_client/test/tft_bridge_test.dart
@@ -36,7 +36,7 @@ void main() {
             target: "GDHJP6TF3UXYXTNEZ2P36J5FH7W4BJJQ4AYYAXC66I2Q2AH5B6O6BCFG",
             amount: 0);
       } catch (error) {
-        expect(error, isNotNull);
+        expect(error, contains("AmountIsLessThanWithdrawFee"));
       }
     });
 
