0d61bb015301f571cab78acc3231b9b6d7ba02e1
AlaaElattar <alaamahmoud.1223@gmail.com>
2024-07-16 10:47:41 +0300

    remove provider=null in disconnect

--- packages/tfchain_client/lib/src/client.dart
+++ packages/tfchain_client/lib/src/client.dart
@@ -174,7 +174,6 @@ class Client extends QueryClient {
   @override
   Future<void> disconnect() async {
     await api.disconnect();
-    provider = null;
   }
 
   Uint8List hexToBytes(String hexString) {
