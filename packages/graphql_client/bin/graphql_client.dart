548d6cb974bd3305b6c06608b47de9d21eff25d9
mariammahrous23 <mariammahrous23@gmail.com>
2024-07-31 10:51:05 +0300

    feat: added better example for testing the fns in bin folder

--- packages/graphql_client/bin/graphql_client.dart
+++ packages/graphql_client/bin/graphql_client.dart
@@ -11,28 +11,19 @@ void main() async {
   final graphQLClient = GraphQLClient('https://graphql.dev.grid.tf/graphql');
 
   
-    //await graphQLClient.twins
-      //.TwinsConnections(TwinConnectionsReturnOptions(), TwinConnectionsQueryOptions());
+    await graphQLClient.twins
+      .TwinsConnections(TwinConnectionsReturnOptions(totalCount: true,
+      edges:TwinConnectionsEdgeReturnOptions(cursor: true, node: TwinReturnOptions(accountID: true, gridVersion: true))), 
+      TwinConnectionsQueryOptions(whereOptions: TwinWhereOptions(gridVersionEq: 1), orderby: OrderBy.gridVersion_ASC));
 
-    //await graphQLClient.twins
-     // .Twins(TwinReturnOptions(accountID: true , relay:true , twinID: true),
-      //TwinQueryOptions(orderby: OrderBy.id_ASC , limit: 10),);
-  
-  /*
-
-      await graphQLClient.twins
-      .Twins(TwinReturnOptions(accountID: true , relay:true , twinID: true),
-      TwinQueryOption(orderby: OrderBy.id_ASC , limit: 10),
-);
-    
     await graphQLClient.twins
       .Twins(TwinReturnOptions(accountID: true , relay:true , twinID: true),
-      TwinQueryOption(id_eq: "0010961887-000002-f81a4"));
+      TwinQueryOptions(orderby: OrderBy.id_ASC , limit: 10),);
   
 
-  await graphQLClient.farms
-      .listFarmsByTwinId(ListFarmsByTwinIdOptions(twinId: 214));
-  */
+  //await graphQLClient.farms
+      //.listFarmsByTwinId(ListFarmsByTwinIdOptions(twinId: 214));
+  
 
   // final contracts = await graphQLClient.contracts
   //     .listContractsByTwinId(ListContractByTwinIdOptions(twinId: 5110));
