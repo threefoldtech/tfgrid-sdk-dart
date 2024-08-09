b4edd4543fe283d07a773c9e5ddd675170511946
AlaaElattar <alaamahmoud.1223@gmail.com>
2024-08-09 13:11:46 +0300

    update bin file to fix linting

--- packages/graphql_client/bin/graphql_client.dart
+++ packages/graphql_client/bin/graphql_client.dart
@@ -1,28 +1,11 @@
 import 'package:graphql_client/graphql_client.dart';
 import 'package:graphql_client/models.dart';
-import 'graphql_client.reflectable.dart';
+// import 'graphql_client.reflectable.dart';
 
 void main() async {
-  initializeReflectable();
+  // initializeReflectable();
   final graphQLClient = GraphQLClient('https://graphql.dev.grid.tf/graphql');
 
-  final List<ContractStates> contractStates = [
-    ContractStates.Created,
-    ContractStates.GracePeriod
-  ];
-  final NodeContractReturnOptions returnOptions = NodeContractReturnOptions(
-    id: true,
-    gridVersion: true,
-    contractID: true,
-    twinID: true,
-    state: true,
-    createdAt: true,
-    solutionProviderID: true,
-    nodeID: true,
-    resourcesUsed: ContractUsedResourcesReturnOptions(
-      hru: true,
-    ),
-  );
   final Future<List<ContractBillReports>> contracts = graphQLClient.contracts
       .listContractConsumption(BigInt.from(113803), null);
 
