7e4d60057b04de4f0c036984f435737e36e9d66a
AlaaElattar <alaamahmoud.1223@gmail.com>
2024-08-13 16:17:12 +0300

    make client2 late

--- packages/tfchain_client/test/setup_manager.dart
+++ packages/tfchain_client/test/setup_manager.dart
@@ -122,7 +122,7 @@ class SetupManager {
     _url = Platform.environment['URL'] ?? 'ws://0.0.0.0:9944';
     _type = Platform.environment['KEYPAIR_TYPE'] ?? 'sr25519';
     _relay = "relay.dev.grid.tf";
-    Client client2;
+    late Client client2;
 
     if (_initializeClient) {
       _mnemonic = bip39.generateMnemonic();
