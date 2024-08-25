d3997a4caf9358f5c482a15ddfba74e331e9a803
AlaaElattar <alaamahmoud.1223@gmail.com>
2024-08-25 13:37:32 +0300

    update getRequest to handle both http and https

--- packages/gridproxy_client/bin/gridproxy_client.dart
+++ packages/gridproxy_client/bin/gridproxy_client.dart
@@ -2,7 +2,7 @@ import 'package:gridproxy_client/gridproxy_client.dart';
 import 'package:gridproxy_client/models/farms.dart';
 
 void main() async {
-  GridProxyClient client = GridProxyClient('gridproxy.dev.grid.tf');
+  GridProxyClient client = GridProxyClient('https://gridproxy.dev.grid.tf/');
 
   final options = ListFarmsQueryParameters(farmID: 1);
   final farms = await client.farms.list(options);
--- packages/gridproxy_client/lib/src/client.dart
+++ packages/gridproxy_client/lib/src/client.dart
@@ -21,11 +21,19 @@ class GridProxyClient {
     try {
       final convertedQueryParameters = queryParameters
           ?.map((key, value) => MapEntry(key, value?.toString() ?? ''));
-      final uri = convertedQueryParameters != null &&
+
+      final uri = Uri.parse(baseUrl);
+      final scheme = uri.scheme;
+      final host = uri.host;
+
+      final requestUri = convertedQueryParameters != null &&
               convertedQueryParameters.isNotEmpty
-          ? Uri.https(baseUrl, path, convertedQueryParameters)
-          : Uri.https(baseUrl, path);
-      final response = await http.get(uri);
+          ? (scheme == 'https'
+              ? Uri.https(host, path, convertedQueryParameters)
+              : Uri.http(host, path, convertedQueryParameters))
+          : (scheme == 'https' ? Uri.https(host, path) : Uri.http(host, path));
+      final response = await http.get(requestUri);
+
       if (response.statusCode == 200) {
         final jsonData = json.decode(response.body);
         return jsonData;
