01ebb043fda235eafa432a9a99ce259b28ddd45b
AlaaElattar <alaamahmoud.1223@gmail.com>
2024-08-28 15:43:26 +0300

    wip: adding fromJson with reflector pkg

--- packages/graphql_client/lib/models/reflector.dart
+++ packages/graphql_client/lib/models/reflector.dart
@@ -3,11 +3,12 @@ import 'package:reflectable/reflectable.dart';
 class Reflector extends Reflectable {
   const Reflector()
       : super(
-          invokingCapability,
-          declarationsCapability,
-          typeCapability,
-          reflectedTypeCapability,
-        );
+            invokingCapability,
+            declarationsCapability,
+            typeCapability,
+            reflectedTypeCapability,
+            typeRelationsCapability,
+            libraryCapability);
 }
 
 const reflector = Reflector();
