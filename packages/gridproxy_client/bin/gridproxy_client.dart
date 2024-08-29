c68907cae5a5b469145d0dc10ae1008b3bb178ea
Alaa228 <alaamahmoud.1223@gmail.com>
2024-08-29 15:48:26 +0300

    Delete packages/gridproxy_client/bin/gridproxy_client.dart

--- packages/gridproxy_client/bin/gridproxy_client.dart
+++
@@ -1,7 +0,0 @@
-import 'package:gridproxy_client/gridproxy_client.dart';
-import 'package:gridproxy_client/models/nodes.dart';
-
-void main() async {
-  GridProxyClient client = GridProxyClient('https://gridproxy.dev.grid.tf');
-  await client.nodes.getNodeStatus(GetNodeStatusOptions(nodeID: 72));
-}
