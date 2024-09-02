a5bc71f0723043eac4148b36d78ad407c365a930
AlaaElattar <alaamahmoud.1223@gmail.com>
2024-09-02 13:11:28 +0300

    undo changes in graphql client by mistake

--- packages/graphql_client/bin/graphql_client.dart
+++ packages/graphql_client/bin/graphql_client.dart
@@ -6,23 +6,10 @@ void main() async {
   // initializeReflectable();
   final graphQLClient = GraphQLClient('https://graphql.dev.grid.tf/graphql');
 
-  final queryOptions =
-      FarmsQueryOptions(limit: 10, orderBy: FarmsOrderByOptions.farmID_DESC);
-  final returnOptions = FarmsReturnOptions(
-      id: true,
-      farmID: true,
-      name: true,
-      twinID: true,
-      publicIps: PublicIpsReturnOptions(
-          ip: true, contractId: true, id: true, gateway: true));
+  final Future<List<ContractBillReports>> contracts = graphQLClient.contracts
+      .listContractConsumption(BigInt.from(113803), null);
 
-  final farms =
-      await graphQLClient.farms.listFarms(queryOptions, returnOptions);
-
-  // final Future<List<ContractBillReports>> contracts = graphQLClient.contracts
-  //     .listContractConsumption(BigInt.from(113803), null);
-
-  // for (final contract in await contracts) {
-  //   print(contract);
-  // }
+  for (final contract in await contracts) {
+    print(contract);
+  }
 }
