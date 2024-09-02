c4773cc16954517e24e250b806a7d865a5d400ad
AlaaElattar <alaamahmoud.1223@gmail.com>
2024-09-02 13:05:02 +0300

    update readme file

--- packages/graphql_client/bin/graphql_client.dart
+++ packages/graphql_client/bin/graphql_client.dart
@@ -6,10 +6,23 @@ void main() async {
   // initializeReflectable();
   final graphQLClient = GraphQLClient('https://graphql.dev.grid.tf/graphql');
 
-  final Future<List<ContractBillReports>> contracts = graphQLClient.contracts
-      .listContractConsumption(BigInt.from(113803), null);
+  final queryOptions =
+      FarmsQueryOptions(limit: 10, orderBy: FarmsOrderByOptions.farmID_DESC);
+  final returnOptions = FarmsReturnOptions(
+      id: true,
+      farmID: true,
+      name: true,
+      twinID: true,
+      publicIps: PublicIpsReturnOptions(
+          ip: true, contractId: true, id: true, gateway: true));
 
-  for (final contract in await contracts) {
-    print(contract);
-  }
+  final farms =
+      await graphQLClient.farms.listFarms(queryOptions, returnOptions);
+
+  // final Future<List<ContractBillReports>> contracts = graphQLClient.contracts
+  //     .listContractConsumption(BigInt.from(113803), null);
+
+  // for (final contract in await contracts) {
+  //   print(contract);
+  // }
 }
