4a8c77af3a28f9c6f921a40948e376a7f80e8184
zaelgohary <zainabelgohary19@gmail.com>
2025-01-16 14:18:53 +0200

    Revert script changes

--- packages/stellar_client/bin/stellar_client.dart
+++ packages/stellar_client/bin/stellar_client.dart
@@ -1,8 +1,14 @@
 import 'package:stellar_client/stellar_client.dart';
 
 void main() async {
-  final stellarClient = await Client(NetworkType.PUBLIC,
-      'SCVWI4TCVUPB7EN4I5X5DQ56RV5E3JNFWDY2NHJCBXOMH5WKVCJ4RSGG');
+  final stellarClient = Client.create(NetworkType.PUBLIC);
+  await stellarClient.activateThroughThreefoldService();
+
+  await stellarClient.transfer(
+      destinationAddress: "destination-public-key",
+      amount: "20",
+      currency: "TFT",
+      memoText: "Memo Text");
 
   await stellarClient.getTransactions();
 }
