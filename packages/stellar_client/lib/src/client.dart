390789e876c3899b5a89fcb71aa4374bb318c85b
Alaa Elattar <alaamahmoud.1223@gmail.com>
2025-05-20 13:46:36 +0300

    include cancel, update orders

--- packages/stellar_client/lib/src/client.dart
+++ packages/stellar_client/lib/src/client.dart
@@ -711,6 +711,10 @@ class Client {
       final SubmitTransactionResponse response =
           await _sdk.submitTransaction(transaction);
       if (!response.success) {
+        if (response.extras!.resultCodes!.operationsResultCodes!
+            .contains('op_low_reserve')) {
+          throw Exception('Transaction failed due to low reserve.');
+        }
         logger.e('Transaction failed with result: ${response.resultXdr}');
         return false;
       }
@@ -760,6 +764,10 @@ class Client {
       final SubmitTransactionResponse response =
           await _sdk.submitTransaction(transaction);
       if (!response.success) {
+        if (response.extras!.resultCodes!.operationsResultCodes!
+            .contains('op_low_reserve')) {
+          throw Exception('Transaction failed due to low reserve.');
+        }
         logger.e('Transaction failed with result: ${response.resultXdr}');
         return false;
       }
