6ac0f9d7d2fd787ec92fd2c3a6c4d71cc6c7505a
zaelgohary <zainabelgohary19@gmail.com>
2025-05-22 12:27:37 +0300

    Edit ContractInfo to include nodeId, edit state to be a list

--- packages/gridproxy_client/lib/src/client.dart
+++ packages/gridproxy_client/lib/src/client.dart
@@ -19,29 +19,36 @@ class GridProxyClient {
   Future<dynamic> getRequest(
       String path, Map<String, dynamic>? queryParameters) async {
     try {
-      final convertedQueryParameters = queryParameters
-          ?.map((key, value) => MapEntry(key, value?.toString() ?? ''));
+      final Map<String, String> encodedQueryParams = {};
+      queryParameters?.forEach((key, value) {
+        if (value is List) {
+          encodedQueryParams[key] = value.map((e) => e.toString()).join(',');
+        } else if (value != null) {
+          encodedQueryParams[key] = value.toString();
+        }
+      });
 
       final uri = Uri.parse(baseUrl);
       final scheme = uri.scheme;
       final host = uri.host;
 
-      final requestUri = convertedQueryParameters != null &&
-              convertedQueryParameters.isNotEmpty
+      final requestUri = encodedQueryParams.isNotEmpty
           ? (scheme == 'https'
-              ? Uri.https(host, path, convertedQueryParameters)
-              : Uri.http(host, path, convertedQueryParameters))
+              ? Uri.https(host, path, encodedQueryParams)
+              : Uri.http(host, path, encodedQueryParams))
           : (scheme == 'https' ? Uri.https(host, path) : Uri.http(host, path));
+
       final response = await http.get(requestUri);
 
       if (response.statusCode == 200) {
         final jsonData = json.decode(response.body);
         return jsonData;
       } else {
-        throw Exception('Failed to load data got: ${response.body}');
+        throw Exception(
+            'Failed to load data. Status: ${response.statusCode}, Body: ${response.body}');
       }
     } catch (e) {
-      throw Exception('Error: $e');
+      throw Exception('Error during GET request to $path: $e');
     }
   }
 }
